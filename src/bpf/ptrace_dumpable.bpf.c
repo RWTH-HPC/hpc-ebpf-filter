b7735bb40911d092709129211d85dff85e1d6ef2
Jannik Glückert <Glueckert@itc.rwth-aachen.de>
2026-05-15 11:09:07 +0200

    ptrace_dumpable rocky8 compat

--- src/bpf/ptrace_dumpable.bpf.c
+++ src/bpf/ptrace_dumpable.bpf.c
@@ -10,13 +10,16 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+#ifndef ROCKY_8
 struct {
     __uint(type, BPF_MAP_TYPE_TASK_STORAGE);
     __uint(map_flags, BPF_F_NO_PREALLOC);
     __type(key, u32);
     __type(value, bool);
 } USER_DUMPABLE SEC(".maps");
+#endif
 
+#ifndef ROCKY_8
 SEC("fentry/exit_mm_release")
 int BPF_PROG(exit_mm_record_ptrace, struct task_struct *task) {
     bool dumpable = get_dumpable(task->mm) == SUID_DUMP_USER;
@@ -25,6 +28,7 @@ int BPF_PROG(exit_mm_record_ptrace, struct task_struct *task) {
 
     return 0;
 }
+#endif
 
 SEC("lsm/ptrace_access_check")
 int BPF_PROG(deny_ptrace_access_check, struct task_struct *child,
@@ -35,7 +39,11 @@ int BPF_PROG(deny_ptrace_access_check, struct task_struct *child,
     }
 
     if (child->mm == NULL) {
+#ifdef ROCKY_8
+        bool *dumpable = NULL;
+#else
         bool *dumpable = bpf_task_storage_get(&USER_DUMPABLE, child, NULL, 0);
+#endif
         if (dumpable == NULL || !*dumpable) {
             log_event(uid, PTRACE_DUMPABLE_EXPLOIT, (union OperationDetails){});
             return -EPERM;
